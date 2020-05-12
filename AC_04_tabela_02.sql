5a72b32fa28e64e1b27ca92117b22d989eb43cb6
romulovieira777 <61069548+romulovieira777@users.noreply.github.com>
2020-05-12 20:31:34 -0300

    AC

---
+++ AC_04_tabela_02.sql
@@ -0,0 +1,12 @@
+create table romulo_tabela_2 (
+cnpj char(14) primary key not null,
+empresa varchar(100),
+endereco varchar(100),
+numero char(10),
+bairro varchar(50),
+cep int,
+municipio varchar(50),
+uf varchar(2),
+cnae_primaria  varchar(200),
+natureza_juridica varchar(200)
+);
